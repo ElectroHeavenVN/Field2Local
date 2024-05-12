555b37223e83c5c6907d90f3bd56d0428bf2b07b
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-05-12 13:53:06 +0700

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,120 @@
+﻿using dnlib.DotNet;
+using System;
+using System.Globalization;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
+
+internal class Logger : ILogger
+{
+    internal static bool isVerbose;
+
+    internal static ConsoleColor oldColor;
+
+    internal static void Log(string content, ConsoleColor consoleColor = ConsoleColor.White, bool isWriteLine = true)
+    {
+        oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = consoleColor;
+        if (isWriteLine)
+            Console.WriteLine(content);
+        else
+            Console.Write(content);
+        Console.ForegroundColor = oldColor;
+    }
+
+    internal static void LogVerbose(string content)
+    {
+        if (isVerbose)
+        {
+            oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("[V] " + EscapeString(content));
+            Console.ForegroundColor = oldColor;
+        }
+    }
+
+    internal static void LogInfo(string content)
+    {
+        oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("[I] " + EscapeString(content));
+        Console.ForegroundColor = oldColor;
+    }
+
+    internal static void LogWarning(string content)
+    {
+        oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("![W] " + EscapeString(content));
+        Console.ForegroundColor = oldColor;
+    }
+
+    internal static void LogError(string content)
+    {
+        oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("![E] " + EscapeString(content));
+        Console.ForegroundColor = oldColor;
+    }
+
+    internal static void LogException(Exception content)
+    {
+
+        oldColor = Console.ForegroundColor;
+        LogException(content, false);
+        Console.ForegroundColor = oldColor;
+    }
+
+    static void LogException(Exception content, bool hasInnerException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{(hasInnerException ? " ---> " : "![Ex] ")}{EscapeString(content.GetType().ToString())}: {EscapeString(content.Message)}{Environment.NewLine}");
+        if (content.InnerException != null)
+            LogException(content.InnerException, true);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(EscapeString(content.StackTrace));
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write((content.InnerException == null ? "" : Environment.NewLine + "   --- End of inner exception stack trace ---") + Environment.NewLine);
+    }
+
+    internal static string EscapeString(string str)
+    {
+        string result = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            char c = str[i];
+            if (i < str.Length - 1 && c == '\r' && str[i + 1] == '\n')
+            {
+                result += Environment.NewLine;
+                i++;
+            }
+            else if (c < 32 || c > 126)
+                result += "\\u" + Convert.ToString(c, 16).PadLeft(4, '0').ToUpper();
+            else result += c;
+        }
+        return result;
+    }
+    public void Log(object sender, LoggerEvent loggerEvent, string format, params object[] args)
+    {
+        switch (loggerEvent)
+        {
+            case LoggerEvent.Error:
+                LogError(string.Format(format, args));
+                break;
+            case LoggerEvent.Warning:
+                LogWarning(string.Format(format, args));
+                break;
+            case LoggerEvent.Info:
+                LogInfo(string.Format(format, args));
+                break;
+            case LoggerEvent.Verbose:
+            case LoggerEvent.VeryVerbose:
+                LogVerbose(string.Format(format, args));
+                break;
+        }
+    }
+
+    public bool IgnoresEvent(LoggerEvent loggerEvent)
+    {
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field2Local
+{
+    internal class Program
+    {
+        public static ModuleDefMD currentModule;
+
+        [DllImport("msvcrt.dll")]
+        static extern int system(string cmd);
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+            if (args.Length == 0)
+            {
+                Console.Write("File path: ");
+                args = new string[] { Console.ReadLine().Replace("\"", "") };
+            }
+            currentModule = ModuleDefMD.Load(args[0]);
+            Dictionary<FieldDef, List<MethodDef>> local2Fields = new Dictionary<FieldDef, List<MethodDef>>();
+            foreach (TypeDef type in currentModule.GetTypes().Where(t => t != currentModule.GlobalType && t.DeclaringType != currentModule.GlobalType))
+            {
+                foreach (MethodDef method in type.Methods.Where(m => m.HasBody && m.Body.HasInstructions))
+                {
+                    foreach (Instruction instruction in method.Body.Instructions.Where(i => (i.OpCode == OpCodes.Stsfld || i.OpCode == OpCodes.Ldsfld || i.OpCode == OpCodes.Ldsflda) && i.Operand is FieldDef))
+                    {
+                        if (local2Fields.ContainsKey((FieldDef)instruction.Operand))
+                        {
+                            if (!local2Fields[(FieldDef)instruction.Operand].Contains(method))
+                                local2Fields[(FieldDef)instruction.Operand].Add(method);
+                        }
+                        else
+                            local2Fields.Add((FieldDef)instruction.Operand, new List<MethodDef>() { method });
+                    }
+                }
+            }
+            Dictionary<FieldDef, Local> field2Local = new Dictionary<FieldDef, Local>();
+            foreach (var item in local2Fields.Where(k => k.Value.Count == 1).Select(k => new KeyValuePair<FieldDef, Local>(k.Key, new Local(k.Key.FieldType))))
+                field2Local.Add(item.Key, item.Value);
+
+            foreach (TypeDef type in currentModule.GetTypes().Where(t => t != currentModule.GlobalType && t.DeclaringType != currentModule.GlobalType))
+            {
+                foreach (MethodDef method in type.Methods.Where(m => m.HasBody && m.Body.HasInstructions))
+                {
+                    foreach (Instruction instruction in method.Body.Instructions.Where(i => (i.OpCode == OpCodes.Stsfld || i.OpCode == OpCodes.Ldsfld || i.OpCode == OpCodes.Ldsflda) && i.Operand is FieldDef))
+                    {
+                        if (!field2Local.ContainsKey((FieldDef)instruction.Operand))
+                            continue;
+                        Local local = field2Local[(FieldDef)instruction.Operand];
+                        if (!method.Body.Variables.Contains(local))
+                            method.Body.Variables.Add(local);
+                        instruction.Operand = local;
+                        if (instruction.OpCode == OpCodes.Ldsfld)
+                            instruction.OpCode = OpCodes.Ldloc;
+                        if (instruction.OpCode == OpCodes.Ldsflda)
+                            instruction.OpCode = OpCodes.Ldloca;
+                        if (instruction.OpCode == OpCodes.Stsfld)
+                            instruction.OpCode = OpCodes.Stloc;
+                    }
+                    method.Body.OptimizeBranches();
+                    method.Body.OptimizeMacros();
+                }
+            }
+
+            ModuleWriterOptions moduleWriterOptions = new ModuleWriterOptions(currentModule);
+            string path = Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "-Field2Local" + Path.GetExtension(args[0]);
+            moduleWriterOptions.MetadataLogger = new Logger();
+            try
+            {
+                currentModule.Write(path, moduleWriterOptions);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Logger.LogException(ex);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                moduleWriterOptions.MetadataOptions.Flags |= MetadataFlags.PreserveAll;
+                currentModule.Write(path, moduleWriterOptions);
+            }
+            system("pause");
+        }
+    }
+}
